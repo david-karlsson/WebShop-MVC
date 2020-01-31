c136b1b7ba949215302a37d6c5f17b7d9b760d81
David Karlsson <david.karlsson1@learnet.se>
2020-01-31 13:10:07 +0100

    Add project files.

---
+++ WebShop/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult LoginSuccess()
+        {
+            return View();
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+
+
+        public IActionResult UserBasket()
+        {
+            return PartialView();
+        }
+
+    /*    public IActionResult OnGetPartial() =>
+    new PartialViewResult
+    {
+        ViewName = "_AuthorPartialRP",
+        ViewData = ViewData,
+    };*/
+
+    }
+}
---
+++ WebShop/Models/UserBasket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.Models
+{
+    public class UserBasket
+    {
+        public int FudgeIceCream { get; set; }
+
+        public int ChocolateIceCream { get; set; }
+
+        public int StrawberryIceCream { get; set; }
+
+
+
+
+    }
+}
